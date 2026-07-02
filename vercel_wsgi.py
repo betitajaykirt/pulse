70601c590946155df4f29dee3f972e0b79357fb0
betitajaykirt <betitajaykirt@gmail.com>
2026-07-03 03:05:44 +0800

    Switch to vercel-django preset package to bypass runtime restriction

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -1,3 +1,7 @@
 import os
 from mysite.wsgi import application
+
+# Point Django settings explicitly for the Vercel server environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
 app = application
