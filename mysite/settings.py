4de5c64d83c6c3a92c08302208a5fe0c46adf7b9
betitajaykirt <betitajaykirt@gmail.com>
2026-07-08 23:41:32 +0800

    Configure native os.environ database fallback architecture

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
