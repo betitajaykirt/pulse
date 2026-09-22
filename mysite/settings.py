d84b7309df3e45679bd710f98f086b8e4bf02508
betitajaykirt <betitajaykirt@gmail.com>
2026-09-22 23:10:49 +0800

    fix for render

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', LOCAL_MYSQL_URL),
         conn_max_age=600,
         conn_health_checks=True,
     )
